8fc0d4575cc8cd1bfa4ca42594cae7df54b38331
Abzal <abzal281088@gmail.com>
2021-04-18 18:44:38 +0600

    первый запуск программы

--- platform/src/main/java/MyProject1.java
+++ platform/src/main/java/MyProject1.java
@@ -1,7 +1,6 @@
 public class MyProject1 {
-
-   public static void main(String[] args) {
-	System.out.println("Hi!");
-   }
+  public static void main(String[] args) {
+    System.out.println("Hi!");
+  }
 
 }
